63b9bc1e2e0ab471c7d02ef0779165828417c4dd
M <m1@mishe1.ru>
2026-01-08 12:00:40 +0300

    Добавлен прототип Агента - 3

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,9 @@
 package main
 
-func main() {}
+import (
+	"github.com/xhrobj/go-metrics-and-alerts/internal/agent"
+)
+
+func main() {
+	agent.Run()
+}
---
+++ internal/agent/agent.go
@@ -0,0 +1,29 @@
+package agent
+
+import (
+	"fmt"
+	"time"
+)
+
+var uptime uint = 0
+
+func Run() {
+	for {
+		if uptime%2 == 0 {
+			poll()
+		}
+		if uptime%10 == 0 {
+			report()
+		}
+		time.Sleep(1 * time.Second)
+		uptime++
+	}
+}
+
+func poll() {
+	fmt.Printf("%d poll\n", uptime)
+}
+
+func report() {
+	fmt.Printf("%d report\n", uptime)
+}
