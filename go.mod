d86389b2d6e88ada3c16116d3ca3f60103d99c36
M <m1@mishe1.ru>
2026-01-10 13:54:12 +0300

    Заменен роутер (mux -> chi) - 7

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/xhrobj/go-metrics-and-alerts
 
 go 1.24.0
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,12 +3,13 @@ package router
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/xhrobj/go-metrics-and-alerts/internal/handler"
 )
 
 func New(h *handler.Handler) http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", h.Update)
+	r := chi.NewRouter()
+	r.HandleFunc("/update/*", h.Update)
 
-	return mux
+	return r
 }
