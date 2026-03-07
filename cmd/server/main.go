c3d5cce65098f160e115c1f04f165912028850ce
M <m1@mishe1.ru>
2026-03-07 19:46:16 +0300

    Добавлено логирование в main Сервера - 15

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,8 +6,10 @@ import (
 
 	"github.com/xhrobj/go-metrics-and-alerts/internal/config"
 	"github.com/xhrobj/go-metrics-and-alerts/internal/handler"
+	"github.com/xhrobj/go-metrics-and-alerts/internal/logger"
 	"github.com/xhrobj/go-metrics-and-alerts/internal/repository"
 	"github.com/xhrobj/go-metrics-and-alerts/internal/router"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -19,11 +21,18 @@ func main() {
 func run() error {
 	cfg := config.GetServerConfig()
 
+	zapLogger, err := logger.New()
+	if err != nil {
+		return err
+	}
+
 	repo := repository.NewMemStorage()
 	h := handler.New(repo)
 	r := router.New(h)
 
-	log.Printf("running server on %s ...", cfg.ServerAddr)
+	zapLogger.Info("running server",
+		zap.String("address", cfg.ServerAddr),
+	)
 
 	return http.ListenAndServe(cfg.ServerAddr, r)
 }
