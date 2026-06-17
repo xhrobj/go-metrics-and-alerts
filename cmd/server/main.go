ed06ef71cb5f5117cad66c743a061fee3360418a
M <xhr@mail.ru>
2026-06-17 09:51:01 +0300

    fix: актуализированы example-конфиги

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -333,9 +333,8 @@ func setupAudit(
 	h *handler.Handler,
 	lg *zap.Logger,
 ) (func(), error) {
-	cleanup := func() {
-		// NOTE: освобождать ресурсы не требуется, пока файловый наблюдатель аудита не создан
-	}
+	noop := func() {}
+	cleanup := noop
 
 	if cfg.AuditFile == "" && cfg.AuditURL == "" {
 		return cleanup, nil
